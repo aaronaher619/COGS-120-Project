00117762bdf15f6a26c7ba187900a0a5680ec973
Aaron Hernandez <aah007@ucsd.edu>
2021-03-01 13:04:06 -0800

    layout edited

--- routes/category.js
+++ routes/category.js
@@ -9,7 +9,12 @@ exports.viewCategory = function(request, response){
         "course": course_name,
         "category": category_name,
         "grade": data[course_name]['categories'][category_name]['grade'],
-        "percent": data[course_name]['categories'][category_name]['percent']
+        "percent": data[course_name]['categories'][category_name]['percent'],
+        "current_percent": data[course_name]['categories'][category_name]['current_percent'],
+        "total_percent": data[course_name]['categories'][category_name]['total_percent'],
+        "first": data[course_name]['categories'][category_name]['first'],
+        "second": data[course_name]['categories'][category_name]['second'],
+        "tests": data[course_name]['categories'][category_name]['tests']
     });
 }
 
